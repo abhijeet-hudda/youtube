f26de3afb04c2bc31b15b531c1496f93929c0bcf
abhijeet-hudda <abhijeethudda9509@gmail.com>
2025-12-19 09:13:13 +0530

    add utils files

---
+++ src/utils/APIErrors.js
@@ -0,0 +1,28 @@
+// class APIErrors extends Error{
+//     //constructor(){}
+// }
+
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+
+export {ApiError}
