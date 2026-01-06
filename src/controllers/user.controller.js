3b456c54b36db7753b322dace3fc4ec4e3952769
abhijeet-hudda <abhijeethudda9509@gmail.com>
2026-01-06 10:39:14 +0530

    update user avatar and add channel profile controller using aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -219,9 +219,231 @@ const refreshAccesssToken = asyncHandler(async(req,res)=>{
     }
 
 })
+
+const changeCurrentPassword = asyncHandler(async (req,res)=>{
+    const {oldPassword,newPassword} = req.body
+
+    //check oldPassword correct -> user chahiye for compare => auth middleware req me req.user add karta h 
+    const user = await User.findById(user?._id);
+
+    const isPasswordCorrect = user.isPasswordCorrect(oldPassword);
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"oldPassword is incorrect")
+    }
+    user.password = newPassword;
+    await user.save({validateBeforeSave:false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        {},
+        "Password changed successfully"
+    ))
+})
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "current user fetched successfully"
+    ))
+})
+
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    /*   stps for update account details
+    1. data lo kya update karna h
+    2. validation 
+    3. user find and update
+    4. send response
+    */
+    const {fullname,email} = req.body
+    //agar yha file update krni h to uske alag controller bnao
+    if(!fullname ||!email){
+        throw new ApiError(401,"fullname and email are required")
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set:{
+                fullname,
+                email: email
+            }
+        },
+        {
+            new: true
+            //new : true means updated user return krna h
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        user,
+        "Account details updated successfully"
+    ))
+    
+})
+
+// req.files & req.file => req.files for multiple files & req.file for single file from multer
+
+const updateUserAvatar = asyncHandler(async (req,res)=>{
+    /* steps for update user avatar
+    1. file lo req.file se
+    2. validation file h ya nahi
+    3. upload on cloudinary
+    4. user find and update
+    5. send response
+    */
+    const avatarLocalPath = req.file?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required");
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400,"Error while uploading on avatar")    
+    }
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        user,
+        "avatar updated successfully"
+    ))
+})
+const updateUsercoverImage= asyncHandler(async (req,res)=>{
+    const coverImageLocalPath = req.file?.path;
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400,"coverImage file is required");
+    }
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage.url){
+        throw new ApiError(400,"Error while uploading on coverImage")    
+    }
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                coverImage:coverImage.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        user,
+        "coverImage updated successfully"
+    ))
+})
+
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400,)
+    }
+
+    //yha username se user nikalna nahi padega bcz aggregation pipeline me $match operation use karenge
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:" subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                channelSubscriberCount:{
+                    $size: "$subscribers"
+                },
+                channelSubscribedTO:{
+                    $size: "$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if: {$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+
+            }
+        },
+        {
+            $project:{
+                fullname:1,
+                username:1,
+                email:1,
+                avatar:1,
+                channelSubscriberCount:1,
+                channelSubscribedTO:1,
+                isSubscribed:1,
+                coverImage:1,
+            }
+        }
+    ])
+    //console.log(channel);
+
+    if(!channel?.length){
+        throw new ApiError(404,"channel does not exist")
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        channel[0],
+        "User channel fetched successfully"
+    ))
+})
+
 export {
     userRegister,
     loginUser,
     logoutUser,
-    refreshAccesssToken
+    refreshAccesssToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUsercoverImage,
+    getUserChannelProfile
+    
 }
