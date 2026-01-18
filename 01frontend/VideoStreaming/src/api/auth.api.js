68c68299e2b1cee39b817197b62d4e2438c3c2d6
abhijeet-hudda <abhijeethudda9509@gmail.com>
2026-01-18 19:01:36 +0530

    feat(auth): implement authentication API functions for user management

---
+++ 01frontend/VideoStreaming/src/api/auth.api.js
@@ -0,0 +1,95 @@
+import api from "./axios.instance"
+
+
+async function createAccount(formData){
+    const response = await api.post("/users/register",formData,{
+        headers: {
+        'Content-Type': 'multipart/form-data'
+        }}
+    )
+    //console.log(response)
+    return response.data
+}
+async function login(formData){
+    const response = await api.post("/users/login",formData,{
+        headers: {
+        'Content-Type': 'multipart/form-data'
+        }}
+    );
+    //console.log(response)
+    return response.data
+}
+async function logout(){
+    const response = await api.post("/users/logout");
+    //console.log(response)
+    return response?.data
+}
+async function getCurrentUser(){
+    const response = await api.get("/users/current-user");
+    //console.log(resopnse);
+    return response.data
+}
+async function changePassword(data) {
+    const response = await api.post("/users/change-password",data);
+    //console.log(response);
+    return response.data
+}
+async function updateAccountDetails() {
+    const response = await api.patch("/users/update-account",data);
+    //console.log(response);
+    return response.data;
+}
+async function updateAvatar(avatarFile){
+    const formData = new FormData();
+    formData.append("avatar", avatarFile);
+    
+    const response = await api.patch("/users/avatar",
+        formData,
+        {
+            headers:{
+                "Content-Type":"multipart/form-data"
+            }
+        }
+    )
+
+    return response.data;
+
+}
+async function updateCoverImage(coverImageFile){
+    const formData = new FormData();
+    formData.append("coverImage", coverImageFile);
+    
+    const response = await api.patch("/users/cover-image",
+        formData,
+        {
+            headers:{
+                "Content-Type":"multipart/form-data"
+            }
+        }
+    )
+
+    return response.data;
+
+}
+async function getUserChannelProfile(username){
+    const response = await api.get(`/users/channel/${username}`)
+    //console.log(response)
+    return response.data
+}
+async function refreshAccessToken(){
+  const response = await api.post("/refresh-token");
+  return response.data;
+};
+export {
+    createAccount,
+    login,
+    logout,
+    getCurrentUser,
+    changePassword,
+    updateAccountDetails,
+    updateAvatar,
+    updateCoverImage,
+    getUserChannelProfile,
+    refreshAccessToken
+
+}
